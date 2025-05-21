f5f8f6f0da585859fdca407aaa61e18c191e2c4f
aldairandre99 <nzolageral@gmail.com>
2025-05-21 21:54:09 +0100

    feat: implement todo list functionality with add, edit, and delete features

--- frontend/src/config/site.ts
+++ frontend/src/config/site.ts
@@ -4,26 +4,10 @@ export const siteConfig = {
   name: "Vite + HeroUI",
   description: "Make beautiful websites regardless of your design experience.",
   navItems: [
-    {
+    /* {
       label: "Home",
       href: "/",
-    },
-    {
-      label: "Docs",
-      href: "/docs",
-    },
-    {
-      label: "Pricing",
-      href: "/pricing",
-    },
-    {
-      label: "Blog",
-      href: "/blog",
-    },
-    {
-      label: "About",
-      href: "/about",
-    },
+    }, */
   ],
   navMenuItems: [
     {
@@ -60,7 +44,7 @@ export const siteConfig = {
     },
   ],
   links: {
-    github: "https://github.com/frontio-ai/heroui",
+    github: "https://github.com/aldairandre99/vite-heroui-tauri-template",
     twitter: "https://twitter.com/hero_ui",
     docs: "https://heroui.com",
     discord: "https://discord.gg/9b6yyZKmH4",
--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -3,3 +3,8 @@ import { SVGProps } from "react";
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
   size?: number;
 };
+
+export type Todo = {
+  title : string,
+  description : string
+}
